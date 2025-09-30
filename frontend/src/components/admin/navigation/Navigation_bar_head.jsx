a59e86a037bfe7f1c1194456c9d2740df8abd7a2
RusiruJayanga <rusirujayanga@gmail.com>
2025-09-30 13:15:23 +0530

    Front-end 34
    Admin dashboard is continuing.

--- frontend/src/components/admin/navigation/Navigation_bar_head.jsx
+++ frontend/src/components/admin/navigation/Navigation_bar_head.jsx
@@ -12,7 +12,7 @@ const Navigation_bar_head = () => {
   const token = "token";
   return (
     <div className="z-[10000] text-white">
-      <div className="w-[100%] h-[60px] bg-[#0c0c0c] flex items-center p-[10px] sm:h-[100px] ">
+      <div className="w-[100%] h-[60px] bg-[#1a1a1a] flex items-center p-[10px] sm:h-[100px] ">
         <img
           className="h-[50px] object-cover mr-auto sm:h-[80px] "
           src="logo.png"
@@ -32,7 +32,7 @@ const Navigation_bar_head = () => {
             initial={{ opacity: 0, x: -5, width: 0 }}
             animate={{ opacity: 1, x: 0, width: 180 }}
             transition={{ duration: 0.3 }}
-            className="z-[10100] h-[100vh] bg-[#0c0c0c] right-0 absolute top-0 p-[10px] sm:hidden"
+            className="z-[10100] h-[100vh] bg-[#1a1a1a] right-0 absolute top-0 p-[10px] sm:hidden"
           >
             <h4
               className="w-[40px] h-[40px] flex items-center justify-center cursor-pointer hover:text-[#f21f30] transition-colors duration-300 ease-out"
@@ -67,7 +67,16 @@ const Navigation_bar_head = () => {
                 onClick={() => set_menu_open(!menuOpen)}
                 to="/adminMovies"
               >
-                Movies
+                Now Showing
+              </NavLink>
+              <NavLink
+                className={
+                  "w-[100%] h-[40px] flex items-center justify-center border-b-[2px] border-transparent"
+                }
+                onClick={() => set_menu_open(!menuOpen)}
+                to="/adminMovies"
+              >
+                Upcoming
               </NavLink>
               <NavLink
                 className={
@@ -76,7 +85,7 @@ const Navigation_bar_head = () => {
                 onClick={() => set_menu_open(!menuOpen)}
                 to="/adminAddMovies"
               >
-                Add movies
+                Add Movies
               </NavLink>
               <NavLink
                 className={
@@ -94,7 +103,25 @@ const Navigation_bar_head = () => {
                 onClick={() => set_menu_open(!menuOpen)}
                 to="/adminContact"
               >
+                Bookings
+              </NavLink>
+              <NavLink
+                className={
+                  "w-[100%] h-[40px] flex items-center justify-center border-b-[2px] border-transparent"
+                }
+                onClick={() => set_menu_open(!menuOpen)}
+                to="/adminContact"
+              >
                 Contact
+              </NavLink>
+              <NavLink
+                className={
+                  "w-[100%] h-[40px] flex items-center justify-center border-b-[2px] border-transparent"
+                }
+                onClick={() => set_menu_open(!menuOpen)}
+                to="/adminUser"
+              >
+                Users
               </NavLink>
             </motion.div>
           </motion.div>
--- frontend/src/pages/admin/home/Home.jsx
+++ frontend/src/pages/admin/home/Home.jsx
@@ -2,8 +2,33 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-amber-50">aaaaaaaaa</h1>
+    <div className="text-white">
+      <div className="h-[100vh] w-[250px] bg-[#1a1a1a] sticky top-[100px] left-0 font-light  ">
+        <h5 className="w-[100%] flex items-center justify-center p-[20px] opacity-[0.8] hover:opacity-[1] transition-opacity duration-300 ease-out cursor-pointer  ">
+          Home
+        </h5>
+        <h5 className="w-[100%] flex items-center justify-center p-[20px] opacity-[0.8] hover:opacity-[1] transition-opacity duration-300 ease-out cursor-pointer  ">
+          Now Showing
+        </h5>
+        <h5 className="w-[100%] flex items-center justify-center p-[20px] opacity-[0.8] hover:opacity-[1] transition-opacity duration-300 ease-out cursor-pointer  ">
+          Upcoming
+        </h5>
+        <h5 className="w-[100%] flex items-center justify-center p-[20px] opacity-[0.8] hover:opacity-[1] transition-opacity duration-300 ease-out cursor-pointer  ">
+          Add Movies
+        </h5>
+        <h5 className="w-[100%] flex items-center justify-center p-[20px] opacity-[0.8] hover:opacity-[1] transition-opacity duration-300 ease-out cursor-pointer  ">
+          Seats
+        </h5>
+        <h5 className="w-[100%] flex items-center justify-center p-[20px] opacity-[0.8] hover:opacity-[1] transition-opacity duration-300 ease-out cursor-pointer  ">
+          Bookings
+        </h5>
+        <h5 className="w-[100%] flex items-center justify-center p-[20px] opacity-[0.8] hover:opacity-[1] transition-opacity duration-300 ease-out cursor-pointer  ">
+          Contact
+        </h5>
+        <h5 className="w-[100%] flex items-center justify-center p-[20px] opacity-[0.8] hover:opacity-[1] transition-opacity duration-300 ease-out cursor-pointer  ">
+          Users
+        </h5>
+      </div>
     </div>
   );
 };

dbb35a6917135a10459894c601028941dafb2c40
RusiruJayanga <rusirujayanga@gmail.com>
2025-10-26 22:12:45 +0530

    Quick Update

--- frontend/src/pages/user/details/Details.jsx
+++ frontend/src/pages/user/details/Details.jsx
@@ -15,6 +15,16 @@ const Details = () => {
   const { data: movieDetails, isLoading, isError } = useMovie(movieId);
   const { data: movieRatings } = useMovieWithRatings(movieId);
 
+  //get tickets page
+  const navigate = useNavigate();
+  const handleGetTickets = (movieId) => {
+    if (localStorage.getItem("token")) {
+      navigate(`/booking`, { state: { movieId } });
+    } else {
+      toast.warning("Please log in to book tickets !");
+    }
+  };
+
   //interests function
   const { mutate: handleAddInterest } = useAddUserInterests();
 
@@ -35,9 +45,14 @@ const Details = () => {
           {movieDetails?.title}
         </h2>
         <div className="flex items-center justify-start mt-[10px] gap-[10px] ">
-          <button className="w-[200px] flex bg-[#f21f30] text-white border-[1px] border-[#f21f30] hover:bg-transparent hover:text-[#f21f30]">
-            GET TICKETS
-          </button>
+          {movieDetails?.status === "nowShowing" && (
+            <button
+              className="w-[200px] flex bg-[#f21f30] text-white border-[1px] border-[#f21f30] hover:bg-transparent hover:text-[#f21f30]"
+              onClick={() => handleGetTickets(movieDetails?._id)}
+            >
+              GET TICKETS
+            </button>
+          )}
           <button
             className="flex w-[40px] border-[1px] text-white pt-[3px] border-white hover:bg-white hover:text-black"
             onClick={() => handleAddInterest(movieDetails?._id)}
@@ -82,17 +97,15 @@ const Details = () => {
               ))}
             </div>
             <div>
-              <div className=" flex items-center justify-center gap-[10px]">
+              <div className=" flex items-center justify-center gap-[5px]">
                 <img className="w-[60px] " src="rating/imdb.png" alt="rating" />
-                <h4>{movieRatings?.externalRatings?.imdb || "N/A"}/10</h4>
+                <h4 className="w-[60px]">
+                  {movieRatings?.externalRatings?.imdb || "N/A"}/10
+                </h4>
               </div>
-              <div className=" flex items-center justify-center gap-[10px] md:justify-between md:w-[110px]">
-                <img
-                  className="w-[50px] mr-[15px]"
-                  src="rating/roten.png"
-                  alt="rating"
-                />
-                <h4>
+              <div className=" flex items-center justify-center gap-[5px]">
+                <img className="w-[60px]" src="rating/roten.png" alt="rating" />
+                <h4 className="w-[60px]">
                   {movieRatings?.externalRatings?.rottenTomatoes || "N/A"}
                 </h4>
               </div>
@@ -103,6 +116,29 @@ const Details = () => {
           <h4 className="text-white font-medium uppercase">
             More Trailers and Images for {movieDetails?.title}
           </h4>
+          <div className="w-[100%] flex flex-col items-center justify-center gap-[20px] mt-[40px] md:flex-row xl:justify-start">
+            <video
+              controls
+              className="w-[350px] h-[200px] rounded-[5px] md:h-[220px] xl:h-[200px]"
+            >
+              <source
+                src={movieDetails?.trailerUrl || "default_trailer.mp4"}
+                type="video/mp4"
+              />
+            </video>
+            <div className="flex flex-col items-center justify-center gap-[20px] md:w-[220px] xl:w-auto xl:flex-row">
+              <img
+                className="w-[350px] h-[200px] rounded-[5px] object-cover md:w-[200px] md:h-[100px] xl:w-[350px] xl:h-[200px] xl:opacity-[0.8] hover:opacity-[1] transition duration-300 ease-out "
+                src={movieDetails?.galleryImages[0] || "default_movie.jpg"}
+                alt={movieDetails?.title}
+              />
+              <img
+                className="w-[350px] h-[200px] rounded-[5px] object-cover md:w-[200px] md:h-[100px] xl:w-[350px] xl:h-[200px] xl:opacity-[0.8] hover:opacity-[1] transition duration-300 ease-out "
+                src={movieDetails?.galleryImages[1] || "default_movie.jpg"}
+                alt={movieDetails?.title}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>

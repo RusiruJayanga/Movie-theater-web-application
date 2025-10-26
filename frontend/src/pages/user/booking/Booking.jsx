cf25cc1cd22fe808d492d4b3b6ce83c1bad36c1e
RusiruJayanga <rusirujayanga@gmail.com>
2025-10-26 22:12:52 +0530

    Quick Update

--- frontend/src/pages/user/booking/Booking.jsx
+++ frontend/src/pages/user/booking/Booking.jsx
@@ -15,7 +15,7 @@ const rows = [
   { row: "F", seats: 10 },
   { row: "G", seats: 10 },
 ];
-const seatPrice = 500;
+const seatPrice = 1000;
 
 const Booking = () => {
   //movie id
@@ -41,113 +41,123 @@ const Booking = () => {
   const totalPrice = selectedSeats.length * seatPrice;
 
   return (
-    <div className="w-[90%] mx-auto min-h-screen text-white flex flex-col items-center mt-[40px] cursor-default xl:mt-[20px] xl:w-[1240px] ">
-      <div className="w-[100%] bg-[#1a1a1a] rounded-[20px] p-[10px] mt-[60px] md:items-center md:p-[20px] md:flex md:justify-between ">
-        <div className="w-[100%] flex items-start justify-start gap-[10px] font-extralight ">
-          <img
-            className="w-[150px] h-[250px] object-cover rounded-[10px] md:w-[200px] md:h-auto xl:w-[250px] xl:h-[320px] "
-            src={movieDetails?.poster || "default_movie.jpg"}
-            alt={movieDetails?.title}
-          />
-          <div className="hidden xl:flex flex-col items-center justify-center gap-[10px] w-[250px] ">
+    <div className="w-[100%] p-[10px] text-[#eeeeee] font-light mt-[20px] cursor-default xl:mt-[20px] xl:w-[1240px] xl:mx-auto ">
+      <div className="md:flex gap-[20px] ">
+        <div>
+          <div className="flex gap-[20px] ">
             <img
-              className="w-[250px] h-[160px] object-cover rounded-[10px]"
-              src={movieDetails?.galleryImages[0] || "default_movie.jpg"}
+              className="w-[70px] h-[70px] rounded-full md:w-[90px] md:h-[90px] "
+              src={movieDetails?.poster || "default_movie.jpg"}
               alt={movieDetails?.title}
             />
-            <img
-              className="w-[250px] h-[160px] object-cover rounded-[10px]"
-              src={movieDetails?.galleryImages[1] || "default_movie.jpg"}
-              alt={movieDetails?.title}
-            />
-          </div>
-          <div className="flex flex-col ml-[10px] ">
-            <h5 className="w-[100%] font-light uppercase ">
-              {movieDetails?.title}
-            </h5>
-            <p className="capitalize font-light mt-[20px] opacity-[0.8]">
-              {formatDuration(movieDetails?.runtime)}
-            </p>
-            <h5 className="mt-[5px] text-[#f21f30] uppercase font-bold">
-              {movieDetails?.ratingCategory}
-            </h5>
-            <p className="opacity-[0.8] font-light mt-[10px]">Selected Seats</p>
-            <h5 className="w-[100%] font-light p-[5px] border-t-[1px] border-b-[1px] border-[#bdbdbd]/30 mt-[5px]">
-              {selectedSeats.length > 0 ? selectedSeats.join(", ") : "None"}
-            </h5>
-            <p className="opacity-[0.8] mt-[10px]">Total Price</p>
-            <h5 className="w-[100%] p-[5px] font-light border-t-[1px] border-b-[1px] border-[#bdbdbd]/30 mt-[5px]">
-              Rs.{totalPrice}.00
-            </h5>
-            <p className="w-[100%] lowercase h-[30px] text-[#000000] font-extralight">
-              console.error;
-            </p>
-            <button
-              className="flex w-[150px] font-light bg-[#f21f30] border-[1px] border-[#f21f30] hover:bg-[#1a1a1a] hover:text-[#f21f30]"
-              to="/booking"
-            >
-              BOOK
-            </button>
-          </div>
-        </div>
-        <div className="w-[100%] flex flex-col items-start justify-between gap-[10px] mt-[20px] font-light md:mt-0 md:w-[350px] ">
-          {/* repeat */}
-          {showTimeDetails?.map((showtime) => (
-            <div className="w-[100%] ">
-              <div>
-                <input
-                  type="radio"
-                  id={`date-${showtime?._id}`}
-                  value=""
-                  class="hidden peer"
-                  required=""
-                />
-                <label
-                  for={`date-${showtime?._id}`}
-                  class="flex items-center justify-center w-[100%] p-[10px] opacity-[0.8] bg-[#303030] border-[1px] border-[#303030] rounded-[10px] cursor-pointer peer-checked:border-[#bdbdbd] hover:opacity-[1] peer-checked:opacity-[1] transition-opacity duration-300 ease-out "
-                >
-                  <h5>
-                    {showtime?.date} {showtime?.time}{" "}
-                  </h5>
-                </label>
-              </div>
+            <div>
+              <h2 className="font-medium text-white uppercase ">
+                {movieDetails?.title}
+              </h2>
+              <p>
+                <span className="capitalize">
+                  {formatDuration(movieDetails?.runtime) || "N/A"}
+                </span>
+                <span className="ml-[10px] pl-[10px] border-l-[2px] border-[#bdbdbd] text-[#f21f30] opacity-[1] font-bold ">
+                  {movieDetails?.ratingCategory || "N/A"}
+                </span>
+              </p>
             </div>
-          ))}
-          {/* repeat */}
+          </div>
+          <h4 className="text-white font-medium mt-[40px]">
+            <i className="bi bi-geo-alt"></i> AMC MATARA PORT
+          </h4>
+          <div className="w-[320px] mt-[20px] flex flex-wrap gap-[10px] mx-auto md:w-[100%] ">
+            {/* repeat */}
+            {showTimeDetails?.map((showtime) => (
+              <label
+                className="w-[150px] h-[40px] rounded-[20px] border-[1px] cursor-pointer border-[#bdbdbd]/50 flex items-center justify-center transition-colors duration-300 ease-out"
+                key={showtime?._id}
+              >
+                <p className="text-[#bdbdbd]">
+                  {showtime?.date} {showtime?.time}
+                </p>
+              </label>
+            ))}
+            {/* repeat */}
+          </div>
         </div>
+        {/* summery */}
+        <div className=" hidden md:block w-[300px] xl:ml-auto xl:w-[400px] ">
+          <h4 className="text-white font-medium">SUMMARY</h4>
+          <p className="text-[#bdbdbd] mt-[10px]">Selected Seats</p>
+          <h5 className="w-[100%] font-light p-[5px] border-t-[1px] border-b-[1px] border-[#bdbdbd]/50 mt-[5px]">
+            {selectedSeats.length > 0 ? selectedSeats.join(", ") : "None"}
+          </h5>
+          <p className="text-[#bdbdbd] mt-[10px]">Total Price</p>
+          <h5 className="w-[100%] p-[5px] font-light border-t-[1px] border-b-[1px] border-[#bdbdbd]/50 mt-[5px]">
+            Rs.{totalPrice}.00
+          </h5>
+          <p className="w-[100%] lowercase h-[30px] text-[#000000] font-extralight">
+            console.error;
+          </p>
+          <button
+            className="flex w-[150px] bg-[#f21f30] text-white border-[1px] border-[#f21f30] hover:bg-[#1a1a1a] hover:text-[#f21f30]"
+            to="/booking"
+          >
+            BOOK
+          </button>
+        </div>{" "}
       </div>
+      <div className="mt-[60px]">
+        {/* screen */}
+        <div className="w-[90%] mx-auto h-[10px] rounded-tl-[100%] rounded-tr-[100%] bg-[#bdbdbd] my-6 relative xl:w-[70%] "></div>
 
-      {/* screen */}
-      <div className="w-[100%] h-[10px] rounded-tl-[100%] rounded-tr-[100%] bg-[#bdbdbd] my-6 relative xl:w-[70%] "></div>
+        {/* seat grid */}
+        <div className="w-[100%] flex flex-col gap-[20px] mt-[20px] xl:mt-[60px]">
+          {rows.map((row) => (
+            <div key={row.row} className="flex justify-center gap-[10px]">
+              {Array.from({ length: row.seats }).map((_, i) => {
+                const seatId = `${row.row}${i + 1}`;
+                const seatMargin = i === 4 ? "md:mr-[40px]" : "";
+                const isSelected = selectedSeats.includes(seatId);
+                const seatClass = `seat ${seatMargin}`;
 
-      {/* seat grid */}
-      <div className="w-[100%] flex flex-col gap-[20px] mt-[20px] xl:mt-[60px]">
-        {rows.map((row) => (
-          <div key={row.row} className="flex justify-center gap-[10px]">
-            {Array.from({ length: row.seats }).map((_, i) => {
-              const seatId = `${row.row}${i + 1}`;
-              const seatMargin = i === 4 ? "md:mr-[40px]" : "";
-              const isSelected = selectedSeats.includes(seatId);
-              const seatClass = `seat ${seatMargin}`;
-
-              return (
-                <div
-                  key={seatId}
-                  onClick={() => toggleSeat(seatId)}
-                  className={`w-[60px] h-[40px] border-[2px] rounded-[5px] text-[14px] flex items-center cursor-pointer font-extralight justify-center transition duration-300 ease-out hover:border-[#bdbdbd] md:rounded-tl-[40%] md:rounded-tr-[40%]
+                return (
+                  <div
+                    key={seatId}
+                    onClick={() => toggleSeat(seatId)}
+                    className={`w-[60px] h-[40px] border-[2px] rounded-[5px] text-[14px] flex items-center cursor-pointer font-extralight justify-center transition duration-300 ease-out hover:border-[#bdbdbd] md:rounded-tl-[40%] md:rounded-tr-[40%]
                     ${
                       isSelected
                         ? "bg-[#f21f30] border-[#f21f30]"
                         : "border-[#bdbdbd]/50 "
                     }
                     ${seatClass}`}
-                >
-                  {seatId}
-                </div>
-              );
-            })}
-          </div>
-        ))}
+                  >
+                    {seatId}
+                  </div>
+                );
+              })}
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* summery */}
+      <div className="w-[90%] mx-auto mt-[60px] md:hidden ">
+        <h4 className="text-white font-medium">SUMMARY</h4>
+        <p className="text-[#bdbdbd] mt-[10px]">Selected Seats</p>
+        <h5 className="w-[100%] font-light p-[5px] border-t-[1px] border-b-[1px] border-[#bdbdbd]/30 mt-[5px]">
+          {selectedSeats.length > 0 ? selectedSeats.join(", ") : "None"}
+        </h5>
+        <p className="text-[#bdbdbd] mt-[10px]">Total Price</p>
+        <h5 className="w-[100%] p-[5px] font-light border-t-[1px] border-b-[1px] border-[#bdbdbd]/30 mt-[5px]">
+          Rs.{totalPrice}.00
+        </h5>
+        <p className="w-[100%] lowercase h-[30px] text-[#000000] font-extralight">
+          console.error;
+        </p>
+        <button
+          className="flex w-[150px] bg-[#f21f30] text-white border-[1px] border-[#f21f30] hover:bg-[#1a1a1a] hover:text-[#f21f30]"
+          to="/booking"
+        >
+          BOOK
+        </button>
       </div>
     </div>
   );

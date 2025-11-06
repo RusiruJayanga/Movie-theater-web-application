6ecc84bd979bc002d070ca9d35aa41702d4200ab
RusiruJayanga <rusirujayanga@gmail.com>
2025-11-06 22:38:08 +0530

    Quick Update

---
+++ backend/controllers/user/Ticket.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import Booking from "../../models/common/Booking.js";
+
+export const getUserTickets = async (req, res) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      const booking = await Booking.find({ userId: decoded.id })
+        .populate("movieId", "title")
+        .exec();
+
+      res.status(200).json(booking);
+    } catch (error) {
+      console.error("Error fetching user tickets:", error);
+      res.status(500).json({ message: "Internal server error !" });
+    }
+  }
+};

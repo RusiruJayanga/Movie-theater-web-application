85cf76d5990c6c48fa40b55aae1a6457ed823bdd
RusiruJayanga <rusirujayanga@gmail.com>
2025-11-16 22:49:46 +0530

    Quick Update

--- backend/controllers/user/Account.js
+++ backend/controllers/user/Account.js
@@ -13,6 +13,10 @@ export const protect = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+      if (!decoded.id) {
+        return res.status(401).json({ message: "Not authorized, no token !" });
+      }
+
       //find user
       req.user = await User.findById(decoded.id).select("-password");
       next();

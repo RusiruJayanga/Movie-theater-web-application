9b29c715a1cb61d4e48035b533b0dbe2199df19f
RusiruJayanga <rusirujayanga@gmail.com>
2025-11-27 22:34:26 +0530

    Quick Update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.jsx";
 
-//client
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(

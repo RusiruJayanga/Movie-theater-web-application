18bf05ff139de1bd04c6a29cb8daf2ddad9154bf
RusiruJayanga <rusirujayanga@gmail.com>
2025-11-30 22:37:05 +0530

    Loading
    Loading screen is updated.

--- frontend/src/hooks/common/Loading.jsx
+++ frontend/src/hooks/common/Loading.jsx
@@ -1,28 +1,29 @@
 import React from "react";
-import { CircularProgress, Box } from "@mui/material";
+import { Box } from "@mui/material";
 
 //loading screen
 const Loading = () => {
   return (
-    <div>
-      <Box
-        sx={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          background: "rgba(0, 0, 0, 0.6)",
-          display: "flex",
-          alignItems: "center",
-          color: "#f21f30",
-          justifyContent: "center",
-          zIndex: "1000000",
-        }}
-      >
-        <CircularProgress size={70} thickness={3} color="inherit" />
-      </Box>
-    </div>
+    <Box
+      sx={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        background: "rgba(0, 0, 0, 0.8)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        zIndex: "1000000",
+      }}
+    >
+      <img
+        className="w-[100px] h-[100px] object-cover"
+        src="gif/loader.gif"
+        alt="loader"
+      />
+    </Box>
   );
 };
 

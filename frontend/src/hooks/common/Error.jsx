244d9604b5670197f8e051729285c1b18cf80662
RusiruJayanga <rusirujayanga@gmail.com>
2025-11-30 22:37:32 +0530

    Error
    Error screen is updated.

--- frontend/src/hooks/common/Error.jsx
+++ frontend/src/hooks/common/Error.jsx
@@ -1,44 +1,49 @@
 import React from "react";
-import { Link, NavLink, useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Box } from "@mui/material";
 
 //error screen
 const Error = () => {
-  //navigate
+  //navigate to home
   const navigate = useNavigate();
   const handleNavigate = () => {
     navigate("/");
   };
 
   return (
-    <div>
-      <Box
-        sx={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          background: "rgba(0, 0, 0, 1)",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          zIndex: "1000000",
-        }}
-      >
-        <div className="flex flex-col items-center justify-center gap-[20px] font-medium">
-          <h5 className="text-white font-light">
-            ❌ Failed to load details. Please try again later.
-          </h5>
-          <button
-            className="w-[100px] flex bg-[#f21f30] text-white border-[1px] border-[#f21f30] hover:bg-transparent hover:text-[#f21f30]"
-            onClick={handleNavigate}
-          >
-            OK
-          </button>
-        </div>
-      </Box>
-    </div>
+    <Box
+      sx={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        background: "rgba(0, 0, 0, 0.8)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        zIndex: "1000000",
+      }}
+    >
+      <div className="flex flex-col items-center justify-center font-medium">
+        <img
+          className="w-[150px] h-[150px] object-cover"
+          src="gif/error.gif"
+          alt="error"
+        />
+        <h1 className="text-white font-bold">Oops !</h1>
+        <hp className="font-light text-[#bdbdbd]">
+          the page you requested was not found
+        </hp>
+
+        <button
+          className="w-[150px] mt-[10px] flex bg-[#f21f30] text-white border-[1px] border-[#f21f30] hover:bg-transparent hover:text-[#f21f30]"
+          onClick={handleNavigate}
+        >
+          GO HOME
+        </button>
+      </div>
+    </Box>
   );
 };
 

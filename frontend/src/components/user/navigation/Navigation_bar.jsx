b75c930648cf6803251fbe9a33c5128b3c01452d
RusiruJayanga <rusirujayanga@gmail.com>
2025-12-01 22:27:58 +0530

    Navigation bar

--- frontend/src/components/user/navigation/Navigation_bar.jsx
+++ frontend/src/components/user/navigation/Navigation_bar.jsx
@@ -1,35 +1,54 @@
 import React from "react";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navigation_bar = () => {
   return (
     <div className="hidden sm:flex z-[10000] sticky top-0 w-full h-[60px] bg-transparent items-center justify-center gap-[20px] font-light text-white nav-scroll">
       <NavLink
-        className={
-          "w-[80px] h-[40px] flex items-center justify-center border-b-[2px] border-transparent hover:text-[#f21f30] transition-colors duration-300 ease-out active_nav"
-        }
+        className="w-[80px] h-[40px] flex items-center justify-center border-b-[2px]"
+        style={({ isActive }) => {
+          return {
+            borderBottomColor: isActive ? "#f21f30" : "transparent",
+            color: isActive ? "#f21f30" : "white",
+          };
+        }}
         to="/"
-        onClick={() => set_active_nav(" ")}
       >
-        Home
+        <p className="hover:text-[#f21f30] transition-colors duration-300 ease-out">
+          Home
+        </p>
       </NavLink>
       <NavLink
         className={
-          "w-[80px] h-[40px] flex items-center justify-center border-b-[2px] border-transparent hover:text-[#f21f30] transition-colors duration-300 ease-out"
+          "w-[80px] h-[40px] flex items-center justify-center border-b-[2px]"
         }
-        to="/upcoming"
-        onClick={() => set_active_nav(" ")}
+        style={({ isActive }) => {
+          return {
+            borderBottomColor: isActive ? "#f21f30" : "transparent",
+            color: isActive ? "#f21f30" : "white",
+          };
+        }}
+        to="/showtime"
       >
-        Upcoming
+        <p className="hover:text-[#f21f30] transition-colors duration-300 ease-out">
+          Showtimes
+        </p>
       </NavLink>
       <NavLink
         className={
-          "w-[80px] h-[40px] flex items-center justify-center border-b-[2px] border-transparent hover:text-[#f21f30] transition-colors duration-300 ease-out"
+          "w-[80px] h-[40px] flex items-center justify-center border-b-[2px]"
         }
+        style={({ isActive }) => {
+          return {
+            borderBottomColor: isActive ? "#f21f30" : "transparent",
+            color: isActive ? "#f21f30" : "white",
+          };
+        }}
         to="/contact"
-        onClick={() => set_active_nav(" ")}
       >
-        Contact Us
+        <p className="hover:text-[#f21f30] transition-colors duration-300 ease-out">
+          Contact Us
+        </p>
       </NavLink>
     </div>
   );
